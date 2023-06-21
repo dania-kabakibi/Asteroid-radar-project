923100eb02e6eaf9df6128cba5682ddd1aac6fa1
dania-kabakibi <daniakaba99@gmail.com>
2023-06-21 19:11:11 -0400

    code_v3

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,10 +1,27 @@
 package com.udacity.asteroidradar
 
+import com.udacity.asteroidradar.Constants.API_QUERY_DATE_FORMAT
+import java.text.SimpleDateFormat
+import java.util.*
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
-    const val START_DATE = "2015-09-07"
-    const val END_DATE = "2015-09-08"
+    val START_DATE = getNextSevenDaysFormattedDates().get(index = 0)
+    val END_DATE = getNextSevenDaysFormattedDates().get(index = getNextSevenDaysFormattedDates().lastIndex)
     const val API_KEY = "Pkw1MKy8JT3BPpDPd91Eic2bMMNrs3RVivRAmJW7"
     const val BASE_URL = "https://api.nasa.gov/"
+}
+
+private fun getNextSevenDaysFormattedDates(): ArrayList<String> {
+    val formattedDateList = ArrayList<String>()
+
+    val calendar = Calendar.getInstance()
+    for (i in 0..Constants.DEFAULT_END_DATE_DAYS) {
+        val currentTime = calendar.time
+        val dateFormat = SimpleDateFormat(API_QUERY_DATE_FORMAT, Locale.getDefault())
+        formattedDateList.add(dateFormat.format(currentTime))
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+    }
+    return formattedDateList
 }
